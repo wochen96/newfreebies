d22949c9bf02861e2bbf4a92e867628d7855641b
Wo Bin Chen <wobin206@gmail.com>
2019-12-03 22:24:07 -0800

    Updates

---
+++ precache-manifest.1a23a9f9bc986018268ba3e8bdd4ae88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d701c7f34cf1594a02953601825fd8",
+    "url": "/freebies/newfreebies/index.html"
+  },
+  {
+    "revision": "fb0f474dd9a78919563e",
+    "url": "/freebies/newfreebies/static/css/2.d70df38f.chunk.css"
+  },
+  {
+    "revision": "d97041d31a46f8f0db93",
+    "url": "/freebies/newfreebies/static/css/main.bf994b3f.chunk.css"
+  },
+  {
+    "revision": "fb0f474dd9a78919563e",
+    "url": "/freebies/newfreebies/static/js/2.c3475d86.chunk.js"
+  },
+  {
+    "revision": "d97041d31a46f8f0db93",
+    "url": "/freebies/newfreebies/static/js/main.29068db9.chunk.js"
+  },
+  {
+    "revision": "104405e328559b9b5942",
+    "url": "/freebies/newfreebies/static/js/runtime-main.a96b362b.js"
+  },
+  {
+    "revision": "c78914c2d2c3b126a9e8b5b05381e32d",
+    "url": "/freebies/newfreebies/static/media/landing-splash.c78914c2.jpg"
+  },
+  {
+    "revision": "b9c1c0636b3f4e16950cd6b6132284c3",
+    "url": "/freebies/newfreebies/static/media/library.b9c1c063.jpg"
+  }
+]);
